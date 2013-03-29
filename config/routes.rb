67df5a4b92ed8b8484ce783dab387f287b594725
Jane Sternbach <janesternbach@gmail.com>
2013-03-29 14:13:42 -0400

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Somethingborrowed::Application.routes.draw do
   root :to => 'home#index'
   resources :items, :only => [:new, :create]
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
