9e4356b1d38f746ad1f08940fde9f9b87d32d08d
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-04-01 10:15:08 -0400

    pull

--- app/models/review.rb
+++ app/models/review.rb
@@ -17,6 +17,7 @@ class Review < ActiveRecord::Base
   belongs_to :reviewable, :polymorphic => true
   belongs_to :user
   validates :title, :presence => true
+  validates :rating, :numericality => {greater_than: 0, less_than_or_equal_to: 5, only_integer: true}
 
   def item
     self.reviewable if self.reviewable.is_a?(Item)
