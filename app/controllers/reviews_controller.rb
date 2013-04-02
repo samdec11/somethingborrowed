8edad88d268bbd8c5dd1dff3d066e3e5a1659b4d
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-04-01 21:01:07 -0400

    modifications

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   def owner_review_choice
-    borrow = Borrow.find(params[:borrow_id])
+    @borrow = Borrow.find(params[:borrow_id])
     @response = params[:when_review]
     if @response == "later"
       Notifications.review_later_message(borrow).deliver
