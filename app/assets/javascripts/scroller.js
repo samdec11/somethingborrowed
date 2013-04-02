4e0056356d8286babcdf4823b86c683e90460606
Jane Sternbach <janesternbach@gmail.com>
2013-04-02 02:43:49 -0400

    styles

--- app/assets/javascripts/scroller.js
+++ app/assets/javascripts/scroller.js
@@ -4,27 +4,18 @@ $(function(){
 
 function overlay()
 {
-  $(this).children().first().remove();
-  var div = $("<div>");
-  div.css("background", "black");
-  div.css("color", "white");
-  div.css("width", "100px").css("height", "100px");
+  var div = $("<div>").addClass("item_hover");
   var a = $("<a>");
   a.attr("href", "/items/"+$(this).data().id);
-  a.text($(this).data().name);
-  var p = $("<p>");
-  p.text($(this).data().description);
-  div.append(a).append(p);
+  a.text($(this).data().owner+"'s "+$(this).data().name);
+  div.append(a);
   $(this).append(div);
+
+
 }
 
 function regular()
 {
   $(this).children().last().remove();
-  var a = $("<a>");
-  a.attr("href", "/items/"+$(this).data().id);
-  var img = $("<img>").attr("src", $(this).data().image).css("width", "100px").css("height", "100px");
-  a.append(img);
-  $(this).append(a);
 
 }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @popular = Item.all.sort_by!{|x| x.num_borrows}.reverse
     @popular = @popular[0,4]
     c = Category.all.select{|x| x.items.length >= 4 }
+    c = c.shuffle
     @category1 = c.pop
     @category2 = c.pop
     @category3 = c.pop
