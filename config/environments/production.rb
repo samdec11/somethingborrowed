64c3d412f0e91839f9713c087579d6ff1e22e8ed
Jane Sternbach <janesternbach@gmail.com>
2013-04-24 14:30:51 -0400

    production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,10 @@ Somethingborrowed::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :ses
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
